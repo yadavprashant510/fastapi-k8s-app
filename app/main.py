80987f6672b73aa6bfdb6f8e8d8818905e8e7a89
Prashant Yadav <yadavprashant510@gmail.com>
2026-04-03 10:18:29 +0530

    Added code for runnig the fastapi app.

--- app/main.py
+++ app/main.py
@@ -9,3 +9,8 @@ def read_root():
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
